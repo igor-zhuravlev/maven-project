008582283cfcc28a3480f3cbc81380ad0966938c
Ihar Zhurauliou <ihar_zhurauliou@epam.com>
2026-05-05 14:03:59 +0400

    mf task 2 impl: rp cloud messaging

--- resource-processor/src/main/java/com/epam/learn/rp/consumer/ResourceUploadedConsumer.java
+++ resource-processor/src/main/java/com/epam/learn/rp/consumer/ResourceUploadedConsumer.java
@@ -1,22 +1,22 @@
-package com.epam.learn.rp.listener;
+package com.epam.learn.rp.consumer;
 
-import com.epam.learn.rp.config.RabbitConfig;
 import com.epam.learn.rp.event.ResourceUploadedEvent;
 import com.epam.learn.rp.service.ResourceMetadataService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+
+import java.util.function.Consumer;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
-public class ResourceEventsListener {
+public class ResourceUploadedConsumer implements Consumer<ResourceUploadedEvent> {
 
     private final ResourceMetadataService resourceMetadataService;
 
-    @RabbitListener(queues = RabbitConfig.RESOURCE_UPLOADED_QUEUE)
-    public void onResourceUploaded(final ResourceUploadedEvent event) {
+    @Override
+    public void accept(final ResourceUploadedEvent event) {
         log.info("Received event: {}", event);
         resourceMetadataService.handle(event);
     }
